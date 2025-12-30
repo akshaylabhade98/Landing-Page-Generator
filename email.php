50133aa57dc289aa5625d737c55aecb964308804
Akshay Labhade <akshay.labhade98@gmail.com>
2025-12-30 12:09:53 +0530

    Fix HTML encoding issues in email.php

--- email.php
+++ email.php
@@ -230,7 +230,7 @@ include "important.php";
             </div>
         </div>
         <div>
-            <p class="mt-5 mb-3 text-muted text-center">&copy; <?php echo date("Y") ?> Arkentech Solutions</p>
+            <p class="mt-5 mb-3 text-muted text-center">&copy; <?php echo date("Y") ?> <?php echo $Company_name ?></p>
         </div>
     </div>
 
@@ -240,5 +240,6 @@ include "important.php";
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
